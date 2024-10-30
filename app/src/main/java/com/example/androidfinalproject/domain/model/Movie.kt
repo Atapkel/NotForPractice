edc4b88976962dc1e61c3a524e137534d7473efe
Kenesbek Zangar <kenesbek.zangar@gmail.com>
2024-10-30 19:33:06 +0500

    Domain model added

---
+++ app/src/main/java/com/example/androidfinalproject/domain/model/Movie.kt
@@ -0,0 +1,22 @@
+package com.example.androidfinalproject.domain.model
+
+import com.example.androidfinalproject.R
+
+data class Movie(
+    val title: String,
+    val category: String,
+    val rate: Double,
+    val isSeen: Boolean = false,
+    val imageId: Int,
+)
+
+object MovieData {
+    val movies = listOf(
+        Movie("Близкие", "драма", 7.8, isSeen = false, imageId = R.drawable.onboard1),
+        Movie("Легенда", "приключения", 8.5, isSeen = true, imageId = R.drawable.onboard1),
+        Movie("Тайна", "ужасы", 6.5, isSeen = false, imageId = R.drawable.onboard1),
+        Movie("Супергерой", "экшн", 9.0, isSeen = true, imageId = R.drawable.onboard1),
+        Movie("Комедия", "комедия", 7.0, isSeen = true, imageId = R.drawable.onboard1),
+        Movie("Мелодрама", "романтика", 6.9, isSeen = false, imageId = R.drawable.onboard1)
+    )
+}
