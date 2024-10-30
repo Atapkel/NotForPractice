3b603c54c309c20cb1217ad58e67d653cad5b059
Kenesbek Zangar <kenesbek.zangar@gmail.com>
2024-10-30 19:07:19 +0500

    Added models

---
+++ app/src/main/java/com/example/androidfinalproject/domain/model/Genre.kt
@@ -0,0 +1,18 @@
+package com.example.androidfinalproject.domain.model
+
+data class Genre(
+    val movies: List<Movie>,
+    val genreTitle: String
+)
+
+object GenreData {
+    val generes = listOf(
+        Genre(movies = MovieData.movies,"Title1"),
+        Genre(movies = MovieData.movies,"Title2"),
+        Genre(movies = MovieData.movies,"Title3"),
+        Genre(movies = MovieData.movies,"Title4"),
+        Genre(movies = MovieData.movies,"Title5"),
+        Genre(movies = MovieData.movies,"Title6"),
+        Genre(movies = MovieData.movies,"Title7"),
+    )
+}
